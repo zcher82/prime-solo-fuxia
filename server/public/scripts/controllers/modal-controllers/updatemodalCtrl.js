36887616c81bb22b573f7408ec73e38c7600f4f3
zeng her <zcher82@gmail.com>
2016-08-02 17:49:48 -0500

    updated modal window on user landing page

--- server/public/scripts/controllers/modal-controllers/updatemodalCtrl.js
+++ server/public/scripts/controllers/modal-controllers/updatemodalCtrl.js
@@ -1,6 +1,16 @@
 app.controller('UpdateModalCtrl', ['$modalInstance', '$scope',
   function ($modalInstance, $scope) {
 
+  $scope.newInfo = {};
+
+  $scope.updateUserInfo = function () {
+    var data = $scope.newInfo;
+    $http.put('/userlanding/update')
+      .then(function(response) {
+        console.log('PUT', response);
+      });
+  }
+
 
   $scope.closeModal = function () {
     $modalInstance.close();
